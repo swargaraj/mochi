b4a5b7a99c76d10fbe1661db670f70334d060252
Run Qi (Jack) Li <li.jack0707@gmail.com>
2024-07-28 11:51:21 -0400

    feat: added dialog pop-up to prompt user to login or register upon clicking on add to favorite button

--- components/anime/anime-list.tsx
+++ components/anime/anime-list.tsx
@@ -10,6 +10,19 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { AnilistResult } from "@/types/anime";
 import { TvMinimal } from "lucide-react";
 
+import { useUser } from "@/app/_components/context";
+import { useState } from "react";
+import { Button } from "@/components/ui/button";
+
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogFooter,
+} from "@/components/ui/dialog";
+
 /**
  * Interface for the SearchResults component.
  * @interface SearchResultsProps
@@ -47,89 +60,132 @@ const listVariants = cva("mt-6 grid grid-cols-2 gap-2 lg:gap-4 select-none", {
  * @returns {JSX.Element} - The rendered component.
  */
 export function AnimeList({ type, list }: SearchResultsProps) {
+
+  const [isDialogOpen, setIsDialogOpen] = useState(false);
+  const user = useUser();
+
+  const isLoggedIn = !!user;
+
   const addToWatchListHandler = (
     e: React.MouseEvent<SVGSVGElement, MouseEvent>,
     id: string
   ) => {
     e.preventDefault();
-    console.log("TODO: Adding", id, "to watch list");
+    if (isLoggedIn){
+      addAnimeToWatchList(id);
+    }
+    else{
+      setIsDialogOpen(true);
+    }
+  }; 
 
-    // TODO: Save watchlist into DB
-  };
+  const addAnimeToWatchList = (id: string) => {
+    // TODO: Implement the actual adding to watch list functionality
+    console.log(`Adding anime with ID ${id} to watch list`);
+  }
+
   return (
-    <div className={cn(listVariants({ type }))}>
-      {/* Map over the anime list and render each anime component */}
-      {list.map((anime, index) => (
-        <div
-          key={index}
-          className="group relative overflow-hidden shadow-lg p-2 rounded-lg"
-        >
-          <Link href={`/anime/${anime.id}`}>
-            <div className="relative">
-              {/* Render the anime image */}
-              <Image
-                loading="lazy"
-                src={anime.image || "default.png"}
-                alt={
-                  anime.title.english || anime.title.userPreferred || "No Title"
-                }
-                width={460}
-                height={650}
-                className="object-cover w-full h-[300px] lg:h-[370px] rounded-lg transition-all hover:opacity-80"
-              />
-              {/* Render the anime details */}
-              <div className="absolute flex w-11/12 justify-between top-2 left-2">
-                <div className="flex">
-                  <div
-                    className={`bg-purple-800 text-white text-xs font-semibold  px-2 py-1 ${
-                      anime.totalEpisodes ? "rounded-tl rounded-bl" : "rounded"
-                    }`}
-                  >
-                    {/* Render the anime type */}
-                    {(anime.type || "").replace(/_/g, " ")}
-                  </div>
-                  {/* Render the anime total episodes */}
-                  {anime.totalEpisodes && (
-                    <div className="bg-teal-600 text-white text-xs font-semibold rounded-tr rounded-br px-2 py-1 flex items-center">
-                      <TvMinimal className="h-3 w-3 mr-1" />
-                      {anime.totalEpisodes}
+    <>
+      <div className={cn(listVariants({ type }))}>
+        {/* Map over the anime list and render each anime component */}
+        {list.map((anime, index) => (
+          <div
+            key={index}
+            className="group relative overflow-hidden shadow-lg p-2 rounded-lg"
+          >
+            <Link href={`/anime/${anime.id}`}>
+              <div className="relative">
+                {/* Render the anime image */}
+                <Image
+                  loading="lazy"
+                  src={anime.image || "default.png"}
+                  alt={
+                    anime.title.english || anime.title.userPreferred || "No Title"
+                  }
+                  width={460}
+                  height={650}
+                  className="object-cover w-full h-[300px] lg:h-[370px] rounded-lg transition-all hover:opacity-80"
+                />
+                {/* Render the anime details */}
+                <div className="absolute flex w-11/12 justify-between top-2 left-2">
+                  <div className="flex">
+                    <div
+                      className={`bg-purple-800 text-white text-xs font-semibold  px-2 py-1 ${
+                        anime.totalEpisodes ? "rounded-tl rounded-bl" : "rounded"
+                      }`}
+                    >
+                      {/* Render the anime type */}
+                      {(anime.type || "").replace(/_/g, " ")}
                     </div>
-                  )}
+                    {/* Render the anime total episodes */}
+                    {anime.totalEpisodes && (
+                      <div className="bg-teal-600 text-white text-xs font-semibold rounded-tr rounded-br px-2 py-1 flex items-center">
+                        <TvMinimal className="h-3 w-3 mr-1" />
+                        {anime.totalEpisodes}
+                      </div>
+                    )}
+                  </div>
+                  <WatchListIcon
+                    id={anime.id}
+                    addToWatchList={addToWatchListHandler}
+                  ></WatchListIcon>
                 </div>
-                <WatchListIcon
-                  id={anime.id}
-                  addToWatchList={addToWatchListHandler}
-                ></WatchListIcon>
+                {/* Render the anime gradient overlay */}
+                <div className="absolute inset-0 bg-gradient-to-t from-10% from-black via-[transparent] to-transparent opacity-70 rounded-lg pointer-events-none"></div>
               </div>
-              {/* Render the anime gradient overlay */}
-              <div className="absolute inset-0 bg-gradient-to-t from-10% from-black via-[transparent] to-transparent opacity-70 rounded-lg pointer-events-none"></div>
-            </div>
-            <div className="absolute bottom-0 left-0 right-0 p-5 text-white">
-              {/* Render the anime title */}
-              <h2 className="text-md lg:text-lg lg:w-[80%] font-semibold leading-5">
-                {(
-                  anime.title.english ||
-                  anime.title.userPreferred ||
-                  "No Title"
-                ).length > 40
-                  ? (
-                      anime.title.english ||
-                      anime.title.userPreferred ||
-                      "No Title"
-                    ).slice(0, 37) + "..."
-                  : anime.title.english ||
+              <div className="absolute bottom-0 left-0 right-0 p-5 text-white">
+                {/* Render the anime title */}
+                <h2 className="text-md lg:text-lg lg:w-[80%] font-semibold leading-5">
+                  {(
+                    anime.title.english ||
                     anime.title.userPreferred ||
-                    "No Title"}
-              </h2>
-              {/* Render the anime release date and status */}
-              <p className="text-sm mt-1">
-                {anime.releaseDate} • {anime.status}
-              </p>
+                    "No Title"
+                  ).length > 40
+                    ? (
+                        anime.title.english ||
+                        anime.title.userPreferred ||
+                        "No Title"
+                      ).slice(0, 37) + "..."
+                    : anime.title.english ||
+                      anime.title.userPreferred ||
+                      "No Title"}
+                </h2>
+                {/* Render the anime release date and status */}
+                <p className="text-sm mt-1">
+                  {anime.releaseDate} • {anime.status}
+                </p>
+              </div>
+            </Link>
+          </div>
+        ))}
+      </div>
+      <Dialog open={isDialogOpen} onOpenChange={setIsDialogOpen}>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Login Required</DialogTitle>
+            <DialogDescription>
+              You need to be logged in to add anime to your watch list. Please log in or sign up.
+            </DialogDescription>
+          </DialogHeader>
+          
+          <DialogFooter className="flex custom-dialog-footer items-center">
+            <Button variant="ghost" onClick={() => setIsDialogOpen(false)}>Cancel</Button>
+            <div className="flex justify-end space-x-2">
+              <div className="gap-2 flex">
+                <Link href={"/join"}>
+                  <Button variant="secondary">
+                    Become a Member
+                  </Button>
+                </Link>
+                <Link href={"/login"}>
+                  <Button>Login</Button>
+                </Link>
+              </div>
             </div>
-          </Link>
-        </div>
-      ))}
-    </div>
+          </DialogFooter>
+        </DialogContent>
+      </Dialog>
+    </>
   );
 }
 
